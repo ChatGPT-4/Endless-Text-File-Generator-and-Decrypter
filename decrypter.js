1a79c2e8c3de12f454a902af737ef21bd55a7023
ChatGPT-4 <127955360+ChatGPT-4@users.noreply.github.com>
2023-03-26 20:40:49 +0200

    Translated into English

--- decrypter.js
+++ decrypter.js
@@ -2,16 +2,19 @@ const fs = require("fs");
 const crypto = require("crypto");
 
 let fileName = "output/file-0.enc";
-let password = "securePassword"; // Passwort zum Entschlüsseln der Datei
+let password = "securePassword";
 
 fs.readFile(fileName, function(err, data) {
   if (err) {
-    console.error(`Fehler beim Lesen der verschlüsselten Datei ${fileName}:`, err);
-  } else {
-    // Entschlüsselung des Textes mit dem Passwort
-    let decipher = crypto.createDecipher("aes-256-cbc", password);
-    let decrypted = Buffer.concat([decipher.update(data), decipher.final()]);
-
-    console.log(`Der originalen Text in der Datei ${fileName} lautet:`, decrypted.toString());
+    console.error(`Error reading file ${fileName}:`, err);
+    process.exit();
   }
+
+  // Decrypt the contents of the file
+  let decipher = crypto.createDecipher("aes-256-cbc", password);
+  let decrypted = decipher.update(data.toString(), "hex", "utf8");
+  decrypted += decipher.final("utf8");
+
+  // Display the original text
+  console.log(`Original text: ${decrypted}`);
 });
